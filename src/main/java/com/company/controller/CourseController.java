60bbda65be9f8033a9de1db05aff4f419c13e81b
MichaelShorokhov <michael.shorokhov@gmail.com>
2018-11-05 19:23:59 +0200

    Mark Crud Finished

--- src/main/java/com/company/controller/CourseController.java
+++ src/main/java/com/company/controller/CourseController.java
@@ -7,81 +7,38 @@ import com.company.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@Controller
+@RestController
+@CrossOrigin
 @RequestMapping(value = "/course")
 public class CourseController {
     @Autowired
     CourseService service;
 
-    @GetMapping (value = "")
-    public String startPage(Model model){
-        model.addAttribute("course", new Course());
-        return "course/courseHomePage";
-    }
-
-    @GetMapping(value = "/find")
-    public String findCourseForm(@ModelAttribute Course course, Model model){
-        model.addAttribute("courses", service.findAll());
-        return "course/findCourse";
-    }
-
-    @PostMapping(value = "/find")
-    public String findCourse(Model model, @ModelAttribute Course course){
-        model.addAttribute("course", service.findCourseById(course.getId()));
-        return "course/findCourse";
-    }
-
     @GetMapping(value = "/findAll")
-    public String findAllCourses(Model model){
+    public List<Course> findAllCourses(){
         List<Course> courses = service.findAll();
-        model.addAttribute("courses", courses);
-        return "course/findAllCourses";
-    }
-
-
-    @GetMapping(value = "/add")
-    public String addCourseForm(Model model){
-        model.addAttribute("course", new Course());
-        return "course/addCourse";
+        return courses;
     }
 
     @PostMapping(value = "/add")
-    public String addCourse(@Valid @ModelAttribute Course course){
+    public Course addCourse(@Valid @RequestBody Course course){
         service.addCourse(course);
-        return "redirect:/course";
+        return course;
     }
 
-    @GetMapping(value = "/remove")
-    public String removeCourseForm(Model model){
-        model.addAttribute("course", new Course());
-        model.addAttribute("courses", service.findAll());
-        return "course/removeCourse";
-    }
-
-    @PostMapping(value = "/remove")
-    public String removeCourse(@ModelAttribute Course course){
-        service.removeCourse(course.getId());
-        return "redirect:/course";
-    }
-
-    @GetMapping(value = "/update")
-    public String updateCourseForm(Model model){
-        model.addAttribute("course", new Course());
-        model.addAttribute("courses", service.findAll());
-        return "course/updateCourse";
+    @DeleteMapping(value = "/remove/{id}")
+    public void removeCourse(@PathVariable String id){
+        service.removeCourse(Long.parseLong(id));
     }
 
     @PostMapping(value = "/update")
-    public String updateCourse(@Valid @ModelAttribute Course course){
+    public Course updateCourse(@Valid @RequestBody Course course){
         service.updateCourse(course);
-        return "redirect:/course";
+        return course;
     }
 }
--- src/main/java/com/company/controller/TermController.java
+++ src/main/java/com/company/controller/TermController.java
@@ -5,94 +5,44 @@ import com.company.service.TermService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@Controller
+@RestController
+@CrossOrigin
 @RequestMapping(value = "/term")
 public class TermController {
     @Autowired
     TermService service;
 
-    @GetMapping (value = "")
-    public String startPage(Model model){
-        model.addAttribute("term", new Term());
-        return "term/termHomePage";
-    }
-
-    @GetMapping(value = "/find")
-    public String findTermForm(@ModelAttribute Term term, Model model){
-        model.addAttribute("terms", service.findAll());
-        return "term/findTerm";
-    }
-
-    @PostMapping(value = "/find")
-    public String findTerm(Model model,@ModelAttribute Term term){
-        model.addAttribute("term", service.findTermById(term.getId()));
-        return "term/findTerm";
-    }
-
     @GetMapping(value = "/findAll")
-    public String findAllTerms(Model model){
+    public List<Term> findAllTerms(){
         List<Term> terms = service.findAll();
-        model.addAttribute("terms", terms);
-        return "term/findAllTerms";
-    }
-
-
-    @GetMapping(value = "/add")
-    public String addTermForm(Model model){
-        model.addAttribute("term", new Term());
-        return "term/addTerm";
+        return terms;
     }
 
     @PostMapping(value = "/add")
-    public String addTerm(@Valid @ModelAttribute Term term){
+    public Term addTerm(@Valid @RequestBody Term term){
         service.addTerm(term);
-        return "redirect:/term";
+        return term;
     }
 
-    @GetMapping(value = "/remove")
-    public String removeTermForm(Model model){
-        model.addAttribute("term", new Term());
-        model.addAttribute("terms", service.findAll());
-        return "term/removeTerm";
-    }
-
-    @PostMapping(value = "/remove")
-    public String removeTerm(@ModelAttribute Term term){
-        service.removeTerm(term.getId());
-        return "redirect:/term";
-    }
-
-    @GetMapping(value = "/update")
-    public String updateTermForm(Model model){
-        model.addAttribute("term", new Term());
-        model.addAttribute("terms", service.findAll());
-        return "term/updateTerm";
+    @DeleteMapping(value = "/remove/{id}")
+    public void removeTerm(@PathVariable String id){
+        service.removeTerm(Long.parseLong(id));
     }
 
     @PostMapping(value = "/update")
-    public String updateTerm(@Valid @ModelAttribute Term term){
+    public Term updateTerm(@Valid @RequestBody Term term){
         service.updateTerm(term);
-        return "redirect:/term";
-    }
-
-    @GetMapping(value = "/close")
-    public String closeTermForm(Model model){
-        model.addAttribute("term", new Term());
-        model.addAttribute("terms", service.findAll());
-        return "term/setClosed";
+        return term;
     }
 
     @PostMapping(value = "/close")
-    public String closeTerm(@ModelAttribute Term term){
+    public Term closeTerm(@RequestBody Term term){
         service.closeTerm(term);
-        return "redirect:/term";
+        return term;
     }
 }
